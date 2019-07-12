4f42c4ed9e62ee5e6c3cf5b0f7e3d97a5be2d0e3
andreamaracani <andreamaracani@gmail.com>
2019-07-12 13:47:23 +0200

    Update Test_CNN_1.py

--- src/Test_CNN_1.py
+++ src/Test_CNN_1.py
@@ -21,7 +21,7 @@ input_path = "data/"
 # tr, va, te, tr_l, va_l, te_l = u.create_dataset_and_split(input_path,  50, 0.7, (97, 40, 3), max_classes=4)
 
 tr, va, te, tr_l, va_l, te_l = u.create_dataset_and_split(input_path, n_samples_test=1, training_percentage = 0.8,
-                                                          sample_shape=(64, 64), number_of_filters=64, addDelta=True,
+                                                          sample_shape=(64, 64, 3), number_of_filters=64, addDelta=True,
                                                           frame_duration=0.03, frame_step=0.015 , max_classes = 6,
                                                           printInfo=True)
 
