edb826ac3f7d436ec8d7f633d6d3cd52bedf292e
andreamaracani <andreamaracani@gmail.com>
2019-07-17 02:01:04 +0200

    Create train3.sh

---
+++ train3.sh
@@ -0,0 +1,10 @@
+python3 src/train.py \
+--datasetpath="data/" \
+--architecture="cnn-trad-fpool3" \
+--batchsize=64 \
+--ckp_folder="checkpoints/" \
+--num_epochs=20 \
+--frame_length=800 \
+--frame_step=500 \
+--target_frame_number=38 \
+--exclude_augmentation=False \
